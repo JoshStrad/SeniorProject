a56ef3e3c761a08983ac6e723364b2232a56b586
Josh S <joshstrad@ah207-3.csci.csuniv.edu>
2024-03-25 15:51:51 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GameWinningStats`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
