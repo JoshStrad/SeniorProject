2a0e0091cacc735323c39a6d2176d89c06e64534
Josh Stradford <joshsj17@gmail.com>
2026-04-22 17:31:43 -0400

    Updates

---
+++ src/pages/more/data-storage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataStoragePage = () => {
+  return (
+    <main>
+      <h1>Data Storage</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+};
+
+export default DataStoragePage;
